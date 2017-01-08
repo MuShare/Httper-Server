61613ce8687ad1ac6d903dad517d85864c7705f6
limeng <lm2343635@126.com>
2017-01-08 15:25:15 +0900

    Change request updateAt to Long.

--- src/main/java/org/fczm/httper/controller/InternetController.java
+++ src/main/java/org/fczm/httper/controller/InternetController.java
@@ -1,12 +1,16 @@
 package org.fczm.httper.controller;
 
+import org.apache.commons.net.whois.WhoisClient;
+import org.fczm.httper.controller.util.ErrorCode;
 import org.fczm.httper.controller.util.ResponseTool;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.HashMap;
 
 @Controller
@@ -29,6 +33,23 @@ public class InternetController {
         return ResponseTool.generateOK(new HashMap<String, Object>(){{
             put("remote", remoteIp);
         }});
+    }
+
+    @RequestMapping(value = "/whois", method = RequestMethod.GET)
+    public ResponseEntity whois(@RequestParam String domain) {
+        WhoisClient whoisClient = new WhoisClient();
+        try {
+            whoisClient.connect(WhoisClient.DEFAULT_HOST);
+            final String result = whoisClient.query(domain);
+            System.out.println(result);
+            whoisClient.disconnect();
+            return ResponseTool.generateOK(new HashMap<String, Object>(){{
+                put("info", result);
+            }});
+        } catch(IOException e) {
+            System.err.println("Error I/O exception: " + e.getMessage());
+            return ResponseTool.generateBadRequest(ErrorCode.ErrorToken);
+        }
     }
 
 }
